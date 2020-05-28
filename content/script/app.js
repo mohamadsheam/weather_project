168a128c4a320e762d4afe0bd10b2ccfac969090
mohammadsheam <mohammadsheam@gmail.com>
2020-05-28 13:25:24 +0600

    config routing

--- content/script/app.js
+++ content/script/app.js
@@ -1,1 +1,15 @@
 var app = angular.module("myApp", ['ngRoute']);
+
+ // configure routing
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/",{
+            templateUrl: "app/layout/main.htm"
+        })
+        .when("/about", {
+            templateUrl: "app/layout/about.htm"
+        })
+        .otherwise({
+            redirectTo: "/"
+        });
+});
