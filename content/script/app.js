81ad33ab4a436c1ec9e0730dca93735942bf9ddb
mohammadsheam <mohammadsheam@gmail.com>
2020-05-28 15:06:37 +0600

    fetch data via API

--- content/script/app.js
+++ content/script/app.js
@@ -4,7 +4,8 @@ var app = angular.module("myApp", ['ngRoute']);
 app.config(function ($routeProvider) {
     $routeProvider
         .when("/",{
-            templateUrl: "app/layout/main.htm"
+            templateUrl: "app/layout/main.htm",
+            controller: 'searchWeatherCtrl'
         })
         .when("/about", {
             templateUrl: "app/layout/about.htm"
@@ -13,3 +14,54 @@ app.config(function ($routeProvider) {
             redirectTo: "/"
         });
 });
+
+// custom value
+app.value("url", "http://api.openweathermap.org/data/2.5/");
+app.value("apiKey", "2494191432201fa79cd1cc06ebeb0139");
+
+// weather Search Controller
+app.controller("searchWeatherCtrl", ["$scope", "$http", "url", "apiKey", function ($scope, $http, url, apiKey) {
+    //console.log(url);
+    //console.log(apiKey);
+    $scope.fetchData = function(city){
+        $scope.data = {};
+        // build url weather?q={city name}&appid={your api key}
+        var api = url + "weather?q=" + city + "&appid="+apiKey ;
+        $http({
+            method: "GET",
+            url : api
+        }).then(function (response) {
+            $scope.data.city = city;
+            $scope.data.status = response.status;
+
+            $scope.data.temp = (response.data.main.temp - 273.15).toFixed(2);
+            $scope.data.weather_main = response.data.weather[0].main;
+            $scope.data.weather_desc = response.data.weather[0].description;
+            $scope.data.weather_icon = response.data.weather[0].icon;
+
+            $scope.data.country = response.data.sys.country;
+            $scope.data.sunrise = formatTime(response.data.sys.sunrise * 1000);
+            $scope.data.sunset = formatTime(response.data.sys.sunset * 1000);
+            $scope.data.currentTime = formatTime(new Date);
+
+            //console.log($scope.data);
+        }, function (reject) {
+            $scope.data = reject.data;
+        });
+
+        // time conversion
+        function formatTime(timeStamp) {
+
+            var date = new Date(timeStamp);
+            var minutes = "0" + date.getMinutes();
+            var seconds = "0" + date.getSeconds();
+            var format = (date.getHours() >= 12) ? "PM" : "AM";
+            var hours = (date.getHours() >= 12) ? date.getHours()-12 : date.getHours();
+            return( hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2) +' ' + format );
+        }
+
+        // empty search field
+        $scope.city = '';
+
+    }
+}]);
